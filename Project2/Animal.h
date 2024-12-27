b70a4edb01230e1856cbccabb1d57a5890981e0e
JangJinWoo <aktsksqkq@gmail.com>
2024-12-27 12:37:59 +0900

    설명에 필요한 곳에 주석 처리

--- Project2/Animal.h
+++ Project2/Animal.h
@@ -1,7 +1,6 @@
-#pragma once
+﻿#pragma once
 class Animal
 {
 public:
-	virtual void makeSound() = 0;
+	virtual void makeSound() = 0; //순수가상함수화 : 파생클래스들은 무조건 제작해야함.
 };
-
--- Project2/First.cpp
+++ Project2/First.cpp
@@ -1,22 +1,21 @@
-#include "Animal.h"
+﻿#include "Animal.h"
 #include "Cat.h"
 #include "Dog.h"
 #include "Cow.h"
 
 void main()
 {
-	Animal* asdf[3];
+	//정적배열 할당
+	Animal* asdf[] = {new Dog(), new Cat(), new Cow()};
 
-	asdf[0] = new Dog();
-	asdf[1] = new Cat();
-	asdf[2] = new Cow();
-
+	//각 객체들의 makeSound() 호출 : 기본클래스인 Animal의 순수가상함수라 없을수는 없다
 	for (int i = 0; i < 3; i++)
 	{
 		if(asdf[i] != nullptr)
 			asdf[i]->makeSound();
 	}
 
+	//동적 할당한 원소들 제거
 	for (int i = 0; i < 3; i++)
 	{
 		delete asdf[i];
